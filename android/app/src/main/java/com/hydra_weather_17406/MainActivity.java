5ed55d8e51870a12bfb6f9b84557652bf2434925
Crowdbotics <team@crowdbotics.com>
2020-05-26 13:35:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hydra_weather_17406/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hydra_weather_17406;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "hydra_weather_17406";
+    }
+}
